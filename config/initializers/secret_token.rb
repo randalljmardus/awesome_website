d74d290eea925f49064af4c4ee3d4ee8fda2be19
Randall Mardus <randallmardus@Randall-Marduss-MacBook.local>
2014-05-06 08:50:57 -0400

    First Bogus Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AwesomeWebsite::Application.config.secret_key_base = 'c7ac654d1abcce940b7d331664be6bda0dcb1fa3eee508b49c71adf1b9d9a3cabb6e3e60fef29c3a842cd37d226443c101a1d1ffbbf1db4bbef1976282867da8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AwesomeWebsite::Application.config.session_store :cookie_store, key: '_awesome_website_session'
